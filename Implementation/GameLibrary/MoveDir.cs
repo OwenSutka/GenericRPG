af2b1ba413c1e67f9da2c22e15748bcb0f34da9e
OwenSutka <owensutka@gmail.com>
2020-02-03 16:45:59 -0600

    Added comments to the MoveDir.cs Enum

--- Implementation/GameLibrary/MoveDir.cs
+++ Implementation/GameLibrary/MoveDir.cs
@@ -1,4 +1,5 @@
 ﻿namespace GameLibrary {
+  // Will be used in Character.cs to define the movement directions in a case statement. Just why would you ever do this...
   public enum MoveDir {
     NO_MOVE,
     UP,
